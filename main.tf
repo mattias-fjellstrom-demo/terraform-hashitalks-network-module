728ddcb3c6ba8ca99993ac8885e94ae3eb13bc4c
Mattias Fjellström <mattias.fjellstrom@gmail.com>
2024-01-23 14:54:29 +0100

    Format

--- main.tf
+++ main.tf
@@ -2,14 +2,12 @@ resource "azurerm_virtual_network" "this" {
   name                = "vnet-${var.name_suffix}"
   resource_group_name = var.resource_group.name
   location            = var.resource_group.location
-
-  address_space = [var.vnet_cidr_range]
-  tags          = var.resource_group.tags
+  address_space       = [var.vnet_cidr_range]
+  tags                = var.resource_group.tags
 }
 
 resource "azurerm_subnet" "this" {
-  for_each = { for subnet in var.subnets : subnet.name => subnet }
-
+  for_each             = { for subnet in var.subnets : subnet.name => subnet }
   name                 = "snet-${each.key}"
   virtual_network_name = azurerm_virtual_network.this.name
   resource_group_name  = var.resource_group.name
